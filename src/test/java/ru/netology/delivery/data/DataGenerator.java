b06c98e54254ff86b3d063c575448ecc7c83faf2
Vyacheslav Vyatkin <Vyatkin.vyacheslav.a@yandex.ru>
2023-12-26 16:37:11 +0500

    commit 26.12

--- src/test/java/ru/netology/delivery/data/DataGenerator.java
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -1,7 +1,7 @@
 package ru.netology.delivery.data;
 
 import com.github.javafaker.Faker;
-//import lombok.Value;
+import lombok.Value;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -9,13 +9,12 @@ import java.util.Locale;
 import java.util.Random;
 
 public class DataGenerator {
-    public static Faker faker = new Faker(new Locale("ru"));
+    //public static Faker faker = new Faker(new Locale("ru"));
 
-   // private DataGenerator() {
-    public DataGenerator() {
-
+    private DataGenerator() {
 
     }
+
 
     public static String generateDate(int shift) {
         return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
@@ -29,12 +28,9 @@ public class DataGenerator {
         return cities[new Random().nextInt(cities.length)];
     }
 
-    public static String generateName() {
-        //var faker = new Faker(new Locale(locale));
-        //Object Name;
-        // Name = Faker.Name();
-        return faker.name().fullName();
-
+    public static String generateName(String locale) {
+        var faker = new Faker(new Locale(locale));
+        return faker.name().lastName() + " " + faker.name().firstName();
 
     }
 
@@ -43,27 +39,22 @@ public class DataGenerator {
         return faker.phoneNumber().phoneNumber();
     }
 
-   // public static Object Faker(Locale ru);
+    public static class Registration {
+        private Registration() {
+        }
 
+        public static UserInfo generateUser(String locale) {
+            return new UserInfo(generateCity(), generateName(locale), generatePhone(locale));
+        }
 
-    //public static class Registration {
-    //private Registration() {
-    // }
-
-
-    public String getCity() {
-        return getCity();
     }
 
-
-
-    public String getPhone() {
-        return getPhone();
+    @Value
+    public static class UserInfo {
+        String city;
+        String name;
+        String phone;
     }
-
-    public String getName() {
-        return getName();
-    }
-
 }
+
 
--- src/test/java/ru/netology/delivery/test/CardDeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/CardDeliveryTest.java
@@ -12,13 +12,13 @@ import java.time.Duration;
 import java.util.Locale;
 
 import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static ru.netology.delivery.data.DataGenerator.faker;
+import ru.netology.delivery.data.DataGenerator.faker;
 
 public class CardDeliveryTest {
 
-    //private Condition[] visible;
 
     @BeforeEach
     void setup() {
@@ -29,21 +29,16 @@ public class CardDeliveryTest {
     @Test
     @DisplayName("Should successful plan meeting")
     void shouldSuccessfulPlanMeeting() {
-        //DataGenerator.UserInfo validUser = DataGenerator.Registration.generateUser("ru");
-        //faker = DataGenerator.Faker(new Locale("ru"));
-        DataGenerator dataGenerator = new DataGenerator();
-        String city = DataGenerator.generateCity();
-        String phone = DataGenerator.generatePhone();
-        String name = DataGenerator.generateName();
+        DataGenerator.UserInfo validUser = DataGenerator.Registration.generateUser("ru");
         int daysToAddForFirstMeeting = 4;
         String firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
         int daysToAddForSecondMeeting = 7;
         String secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
-        $("[data-test-id=city] input").setValue(faker.getCity());
+        $("[data-test-id=city] input").setValue(validUser.getCity());
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("[data-test-id=date] input").setValue(firstMeetingDate);
-        $("[data-test-id=name] input").setValue(faker.getName());
-        $("[data-test-id=phone] input").setValue(faker.getPhone());
+        $("[data-test-id=name] input").setValue(validUser.getName());
+        $("[data-test-id=phone] input").setValue(validUser.getPhone());
         $("[data-test-id=agreement]").click();
         $(byText("Запланировать")).click();
         $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
@@ -58,12 +53,10 @@ public class CardDeliveryTest {
         $("[data-test-id='success-notification'] .notification__content")
                 .shouldHave(exactText("Встреча успешно запланирована на " + secondMeetingDate)).shouldBe(visible);
 
-        }
-
-    private String byText(String запланировать) {
-        return null;
     }
 }
+
+
 
 
 
